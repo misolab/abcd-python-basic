125b0fcc2d41312defbefd591e16e05d4a2fb992
misolab <do.ockhyun@gmail.com>
2018-01-15 13:46:40 +0900

    add module

---
+++ 7_module.py
@@ -0,0 +1,29 @@
+
+# import class_module
+# abcd = class_module.ABCD("abcd")
+
+
+# import class_module as 가나다라
+# abcd = 가나다라.ABCD("abcd")
+
+
+from class_module import ABCD
+abcd = ABCD("abcd")
+
+
+abcd.join("한기갑")
+abcd.join("이호훈")
+abcd.join("황승만")
+abcd.join("수지")
+abcd.join("아이유")
+abcd.join("이효리")
+abcd.join("멍멍이")
+abcd.join("고양이")
+
+abcd.introduce()
+
+abcd.join("코끼리")
+abcd.join("도옥현")
+
+del abcd
+
---
+++ class_module.py
@@ -0,0 +1,51 @@
+
+class ABCD:
+
+    MAX_MEMBER = 10
+
+    def __init__(self, name):
+        self.name = name
+        self.member = ["한성일", "김수진"]
+
+    def join(self, name):
+        if self.member.__len__() < ABCD.MAX_MEMBER:
+            self.member.append(name)
+            print("반갑습니다." + name)
+        else:
+            print(name + "님 죄송합니다.! 정원이 만땅입니다.")
+
+    def leave(self, name):
+        self.member.remove(name)
+        print("또만나요." + name)
+
+    def introduce(self):
+        print("우리 그룹은 " + self.name + "입니다.")
+        print("우리 멤버는 다음과 같습니다")
+        for i, who in enumerate(self.member, 1):
+            print(i.__str__() + "." + who + "님")
+
+    def __del__(self):
+        print("서비스 망했네!!")
+
+
+if __name__ == "__main__":
+    print("여기가 class_module 입니다.")
+
+    abcd = ABCD("abcd")
+
+    abcd.join("한기갑")
+    abcd.join("이호훈")
+    abcd.join("황승만")
+    abcd.join("수지")
+    abcd.join("아이유")
+    abcd.join("이효리")
+    abcd.join("멍멍이")
+    abcd.join("고양이")
+
+    abcd.introduce()
+
+    abcd.join("코끼리")
+    abcd.join("도옥현")
+
+    del abcd
+
